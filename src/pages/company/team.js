bcb87be48ae95131891463c3bb0abf71180e1f82
jenross <ross.m.jennifer@gmail.com>
2020-05-22 16:43:40 -0400

    new about page and two layer path for team page added

---
+++ src/pages/company/team.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Layout from "../../components/layout"
+
+const Team = () => (
+  <Layout>
+    <div>
+      <h1>The Team</h1>
+      <p>
+        I'm baby pok pok gastropub viral microdosing. Twee venmo man braid
+        affogato, intelligentsia asymmetrical unicorn pabst. Swag freegan venmo
+        craft beer thundercats street art tumblr flexitarian leggings subway
+        tile XOXO PBR&B. Cardigan enamel pin locavore air plant bespoke selfies
+        pickled banh mi. Four loko gastropub food truck, cronut meditation
+        direct trade brooklyn poutine flexitarian sustainable narwhal austin
+        beard sriracha.
+      </p>
+    </div>
+  </Layout>
+)
+
+export default Team
