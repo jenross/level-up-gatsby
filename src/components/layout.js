301ded298f10f4303a57dacf5b635e5b12ef398a
jenross <ross.m.jennifer@gmail.com>
2020-05-22 20:47:23 -0400

    began adding styled components

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,7 +11,15 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import Archive from "./archive"
+import styled from "styled-components"
 import "./layout.css"
+
+const MainLayout = styled.div`
+  max-width: 90%;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 4fr 1fr;
+`
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -27,13 +35,8 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+
+      <MainLayout>
         <main>{children}</main>
         <Archive />
         <footer>
@@ -41,7 +44,7 @@ const Layout = ({ children }) => {
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
-      </div>
+      </MainLayout>
     </>
   )
 }
